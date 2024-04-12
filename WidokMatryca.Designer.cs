3e250c2df4bee28ed62c4b17d6b49bc4f28f8358
Grafpl <sergiusz.piorko@gmail.com>
2024-04-12 14:29:03 +0200

    Matryca kolejność

--- WidokMatryca.Designer.cs
+++ WidokMatryca.Designer.cs
@@ -30,33 +30,60 @@
         {
             dataGridView1 = new System.Windows.Forms.DataGridView();
             dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            button1 = new System.Windows.Forms.Button();
+            button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
             // dataGridView1
             // 
             dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new System.Drawing.Point(12, 41);
+            dataGridView1.Location = new System.Drawing.Point(15, 63);
+            dataGridView1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             dataGridView1.Name = "dataGridView1";
             dataGridView1.RowTemplate.Height = 25;
-            dataGridView1.Size = new System.Drawing.Size(1339, 537);
+            dataGridView1.Size = new System.Drawing.Size(1722, 823);
             dataGridView1.TabIndex = 0;
             // 
             // dateTimePicker1
             // 
-            dateTimePicker1.Location = new System.Drawing.Point(12, 12);
+            dateTimePicker1.Location = new System.Drawing.Point(15, 18);
+            dateTimePicker1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             dateTimePicker1.Name = "dateTimePicker1";
-            dateTimePicker1.Size = new System.Drawing.Size(200, 23);
+            dateTimePicker1.Size = new System.Drawing.Size(256, 30);
             dateTimePicker1.TabIndex = 1;
             dateTimePicker1.ValueChanged += dateTimePicker1_ValueChanged;
             // 
+            // button1
+            // 
+            button1.Location = new System.Drawing.Point(366, 18);
+            button1.Name = "button1";
+            button1.Size = new System.Drawing.Size(75, 36);
+            button1.TabIndex = 2;
+            button1.Text = "Gora";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new System.Drawing.Point(447, 19);
+            button2.Name = "button2";
+            button2.Size = new System.Drawing.Size(75, 36);
+            button2.TabIndex = 3;
+            button2.Text = "Dol";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
             // WidokMatryca
             // 
-            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleDimensions = new System.Drawing.SizeF(9F, 23F);
             AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            ClientSize = new System.Drawing.Size(1368, 590);
+            ClientSize = new System.Drawing.Size(1759, 905);
+            Controls.Add(button2);
+            Controls.Add(button1);
             Controls.Add(dateTimePicker1);
             Controls.Add(dataGridView1);
+            Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             Name = "WidokMatryca";
             Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
@@ -67,5 +94,7 @@
 
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
--- WidokMatryca.cs
+++ WidokMatryca.cs
@@ -24,37 +24,24 @@ namespace Kalendarz1
             InitializeComponent();
             DisplayData();
         }
-
         private void DisplayData()
         {
-            DateTime startDate = dateTimePicker1.Value.Date;
-            string query = "SELECT Auta, Dostawca, WagaDek, SztSzuflada FROM dbo.HarmonogramDostaw WHERE DataOdbioru = @StartDate AND Bufor = 'Potwierdzony'";
-
+            // Tworzenie połączenia z bazą danych i pobieranie danych
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
+                // Tworzenie komendy SQL
+                string query = "SELECT Auta, Dostawca, WagaDek, SztSzuflada FROM dbo.HarmonogramDostaw WHERE DataOdbioru = @StartDate AND Bufor = 'Potwierdzony'";
                 SqlCommand command = new SqlCommand(query, connection);
-                command.Parameters.AddWithValue("@StartDate", startDate);
+                command.Parameters.AddWithValue("@StartDate", dateTimePicker1.Value.Date);
 
+                // Tworzenie adaptera danych i wypełnianie DataTable
                 SqlDataAdapter adapter = new SqlDataAdapter(command);
-
                 DataTable table = new DataTable();
                 adapter.Fill(table);
 
-                // Dodanie nowej kolumny do przechowywania numerów wierszy
-                table.Columns.Add("Numer", typeof(int));
-
-                // Iteracja przez wszystkie wiersze i przypisanie numerów
-                for (int i = 0; i < table.Rows.Count; i++)
-                {
-                    table.Rows[i]["Numer"] = i + 1;
-                }
-
-                // Wywołanie funkcji dublowania wierszy
-                DuplicateRows(table);
-
                 // Dodanie pozostałych kolumn do tabeli finalTable
                 DataTable finalTable = new DataTable();
-                finalTable.Columns.Add("Numer", typeof(int));
+                finalTable.Columns.Add("Numer", typeof(int)); // Dodanie kolumny "Numer" na początku
                 finalTable.Columns.Add("Auta", typeof(int));
                 finalTable.Columns.Add("Dostawca", typeof(string));
                 finalTable.Columns.Add("WagaDek", typeof(double));
@@ -67,62 +54,121 @@ namespace Kalendarz1
                 finalTable.Columns.Add("Powrot", typeof(DateTime)); // Kolumna dla powrotu
                 finalTable.Columns.Add("Wozek", typeof(string)); // Kolumna dla wózka
 
-                // Kopiowanie danych z tabeli źródłowej do docelowej
+                int numer = 1; // Początkowa wartość numeru
+
+                // Iteracja przez wiersze tabeli źródłowej
                 foreach (DataRow row in table.Rows)
                 {
-                    DataRow newRow = finalTable.NewRow();
-                    newRow["Numer"] = row["Numer"]; // Skopiuj numer wiersza
-                    newRow["Auta"] = row["Auta"];
-                    newRow["Dostawca"] = row["Dostawca"];
-                    newRow["WagaDek"] = row["WagaDek"];
-                    newRow["SztSzuflada"] = row["SztSzuflada"];
-                    newRow["Kierowca"] = ""; // Pozostaw kolumnę Kierowca pustą
-                    newRow["Pojazd"] = ""; // Pozostaw kolumnę Pojazd pustą
-                    newRow["Naczepa"] = ""; // Pozostaw kolumnę Naczepa pustą
-                    newRow["Wyjazd"] = DBNull.Value; // Pozostaw kolumnę Wyjazd pustą
-                    newRow["Zaladunek"] = DBNull.Value; // Pozostaw kolumnę Zaladunek pustą
-                    newRow["Powrot"] = DBNull.Value; // Pozostaw kolumnę Powrot pustą
-                    newRow["Wozek"] = ""; // Pozostaw kolumnę Wozek pustą
+                    int autaValue = (int)row["Auta"];
 
-                    finalTable.Rows.Add(newRow);
+                    // Duplikowanie wiersza tyle razy, ile wynosi wartość w kolumnie Auta
+                    for (int i = 0; i < autaValue; i++)
+                    {
+                        DataRow newRow = finalTable.NewRow();
+                        newRow["Numer"] = numer++; // Ustaw numer i zwiększ wartość
+                        newRow["Auta"] = row["Auta"];
+                        newRow["Dostawca"] = row["Dostawca"];
+                        newRow["WagaDek"] = row["WagaDek"];
+                        newRow["SztSzuflada"] = row["SztSzuflada"];
+                        newRow["Kierowca"] = ""; // Pozostaw kolumnę Kierowca pustą
+                        newRow["Pojazd"] = ""; // Pozostaw kolumnę Pojazd pustą
+                        newRow["Naczepa"] = ""; // Pozostaw kolumnę Naczepa pustą
+                        newRow["Wyjazd"] = DBNull.Value; // Pozostaw kolumnę Wyjazd pustą
+                        newRow["Zaladunek"] = DBNull.Value; // Pozostaw kolumnę Zaladunek pustą
+                        newRow["Powrot"] = DBNull.Value; // Pozostaw kolumnę Powrot pustą
+                        newRow["Wozek"] = ""; // Pozostaw kolumnę Wozek pustą
+
+                        finalTable.Rows.Add(newRow);
+                    }
                 }
 
+                // Ustawienie źródła danych dla DataGridView
                 dataGridView1.DataSource = finalTable;
-            }
-        }
 
-        // Funkcja do dublowania wierszy
-        // Funkcja do dublowania wierszy
-        private void DuplicateRows(DataTable table)
-        {
-            for (int rowIndex = 0; rowIndex < table.Rows.Count; rowIndex++)
-            {
-                DataRow row = table.Rows[rowIndex];
+                // Tworzenie DataGridViewComboBoxColumn dla kolumny "Kierowca"
+                DataGridViewComboBoxColumn comboBoxColumn = new DataGridViewComboBoxColumn();
+                comboBoxColumn.Name = "Kierowca";
+                comboBoxColumn.HeaderText = "Kierowca";
+                comboBoxColumn.DataPropertyName = "Kierowca";
 
-                int auta = Convert.ToInt32(row["Auta"]);
-                string dostawca = row["Dostawca"].ToString();
-                double wagaDek = Convert.ToDouble(row["WagaDek"]);
-                int sztSzuflada = Convert.ToInt32(row["SztSzuflada"]);
-
-                for (int i = 1; i < auta; i++) // Zaczynamy od 1, bo pierwszy wiersz już istnieje
+                // Pobranie unikalnych wartości kierowców z bazy danych i dodanie ich do ComboBoxa w kolumnie "Kierowca"
+                connection.Open();
+                SqlCommand cmd = new SqlCommand("SELECT Name FROM [LibraNet].[dbo].[Driver]", connection);
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
                 {
-                    DataRow newRow = table.NewRow();
-                    newRow.ItemArray = row.ItemArray; // Skopiuj dane z istniejącego wiersza
-
-                    // Ustaw numer wiersza
-                    newRow["Numer"] = DBNull.Value; // Lub pozostaw go pustym, jeśli nie jest potrzebny
-
-                    table.Rows.Add(newRow);
+                    comboBoxColumn.Items.Add(reader["Name"].ToString());
                 }
+                reader.Close();
+                connection.Close();
+
+                // Dodanie kolumny ComboBox do DataGridView
+                dataGridView1.Columns.Add(comboBoxColumn);
+
             }
         }
 
+        private int selectedRowIndex = -1; // Dodaj zmienną do przechowywania indeksu zaznaczonego wiersza
 
+        // Metoda do przesuwania wiersza w górę
+        // Metoda do przesuwania wiersza w górę
+        private void MoveRowUp()
+        {
+            int rowIndex = dataGridView1.CurrentCell.RowIndex;
+            if (rowIndex > 0)
+            {
+                DataTable table = (DataTable)dataGridView1.DataSource;
+                DataRow row = table.NewRow();
+                row.ItemArray = table.Rows[rowIndex].ItemArray;
+                table.Rows.RemoveAt(rowIndex);
+                table.Rows.InsertAt(row, rowIndex - 1);
+                RefreshNumeration();
+                dataGridView1.Rows[rowIndex - 1].Selected = true; // Zaznacz przesunięty wiersz
+                dataGridView1.Rows[rowIndex].Selected = false; // Odznacz poprzedni zaznaczony wiersz
+                dataGridView1.CurrentCell = dataGridView1.Rows[rowIndex - 1].Cells[0]; // Ustawienie aktywnej komórki na pierwszą kolumnę przesuniętego wiersza
+                selectedRowIndex = rowIndex - 1; // Zaktualizuj indeks zaznaczonego wiersza
+            }
+        }
 
-
+        // Metoda do przesuwania wiersza w dół
+        private void MoveRowDown()
+        {
+            int rowIndex = dataGridView1.CurrentCell.RowIndex;
+            if (rowIndex < dataGridView1.Rows.Count - 1)
+            {
+                DataTable table = (DataTable)dataGridView1.DataSource;
+                DataRow row = table.NewRow();
+                row.ItemArray = table.Rows[rowIndex].ItemArray;
+                table.Rows.RemoveAt(rowIndex);
+                table.Rows.InsertAt(row, rowIndex + 1);
+                RefreshNumeration();
+                dataGridView1.Rows[rowIndex + 1].Selected = true; // Zaznacz przesunięty wiersz
+                dataGridView1.Rows[rowIndex].Selected = false; // Odznacz poprzedni zaznaczony wiersz
+                dataGridView1.CurrentCell = dataGridView1.Rows[rowIndex + 1].Cells[0]; // Ustawienie aktywnej komórki na pierwszą kolumnę przesuniętego wiersza
+                selectedRowIndex = rowIndex + 1; // Zaktualizuj indeks zaznaczonego wiersza
+            }
+        }
+        // Metoda odświeżająca numerację wierszy
+        private void RefreshNumeration()
+        {
+            for (int i = 0; i < dataGridView1.Rows.Count; i++)
+            {
+                dataGridView1.Rows[i].Cells["Numer"].Value = i + 1;
+            }
+        }
         private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
         {
             DisplayData();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MoveRowUp();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MoveRowDown();
         }
     }
 }
